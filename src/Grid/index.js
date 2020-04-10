2f95ae74684d0dcfa2d69093a39cd194bc7ff8da
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-04-10 12:33:19 +0530

    hooks implemented

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,24 +1,43 @@
 import React from "react";
 import Square from "../Square";
-class Grid extends React.Component {
-    getGridRow(offset) {
+// class Grid extends React.Component {
+//     getGridRow(offset) {
+//         return (<div className="grid-row">
+//             <Square value={this.props.squares[offset + 0]} squarePressed={() => this.props.handleClick(offset + 0)}/>
+//             <Square value={this.props.squares[offset + 1]} squarePressed={() => this.props.handleClick(offset + 1)}/>
+//             <Square value={this.props.squares[offset + 2]} squarePressed={() => this.props.handleClick(offset + 2)}/>
+//         </div>);
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <div className="grid">
+//                     {this.props.status}
+//                     {this.getGridRow(0)}
+//                     {this.getGridRow(3)}
+//                     {this.getGridRow(6)}
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+function Grid(props){
+    function getGridRow(offset){
         return (<div className="grid-row">
-            <Square value={this.props.squares[offset + 0]} squarePressed={() => this.props.handleClick(offset + 0)}/>
-            <Square value={this.props.squares[offset + 1]} squarePressed={() => this.props.handleClick(offset + 1)}/>
-            <Square value={this.props.squares[offset + 2]} squarePressed={() => this.props.handleClick(offset + 2)}/>
-        </div>);
+                        <Square value={props.squares[offset + 0]} squarePressed={() => props.handleClick(offset + 0)}/>
+                         <Square value={props.squares[offset + 1]} squarePressed={() => props.handleClick(offset + 1)}/>
+                         <Square value={props.squares[offset + 2]} squarePressed={() => props.handleClick(offset + 2)}/>
+                     </div>);
     }
-    render() {
-        return (
-            <div>
-                <div className="grid">
-                    {this.props.status}
-                    {this.getGridRow(0)}
-                    {this.getGridRow(3)}
-                    {this.getGridRow(6)}
+    return (
+            <div >
+                <div className="grid" >
+                    {props.status}
+                    {getGridRow(0)}
+                    {getGridRow(3)}
+                    {getGridRow(6)}
                 </div>
             </div>
         );
-    }
 }
 export default Grid
--- src/Square/index.js
+++ src/Square/index.js
@@ -1,14 +1,21 @@
 import React from "react";
-
 import './styles.css';
-class Square extends React.Component {
-    render() {
-        return (
+
+// class Square extends React.Component {
+//     render() {
+//         return (
+//             <button className="square-button" onClick={() =>
+//                 this.props.squarePressed()}
+//                    >{this.props.value}</button>
+//         )
+//     }
+// }
+function Square(props){
+    return (
             <button className="square-button" onClick={() =>
-                this.props.squarePressed()}
-                   >{this.props.value}</button>
-        )
-    }
+                props.squarePressed()}
+                   >{props.value}</button>
+        );
 }
 
 export default Square;
